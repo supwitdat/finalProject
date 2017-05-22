09471d89530cec02b94f87d857f657495390e0dd
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-22 15:17:24 -0400

    number rating property, comment property to object

--- public/js/entryCtrl.js
+++ public/js/entryCtrl.js
@@ -2,4 +2,12 @@ var app = angular.module('happyMod');
 
 app.controller("entryController", function($scope, happyService) {
   console.log("entryController has loaded");
+
+	$scope.rating = happyService.getRating();
+	
+	$scope.setEntry = function(comment, mood) {
+		happyService.setEntry(comment);
+		console.log(mood);
+	}
+	
 });
