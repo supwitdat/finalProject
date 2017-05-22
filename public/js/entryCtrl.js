cfd6c63adfc6be6ee2a8c2e8979d1f4b80596f83
nathan-rabens <nathanrabens@gmail.com>
2017-05-22 16:40:27 -0400

    none

--- public/js/entryCtrl.js
+++ public/js/entryCtrl.js
@@ -4,13 +4,13 @@ app.controller("entryController", function($scope, happyService) {
   console.log("entryController has loaded");
 
 	$scope.rating = happyService.getRating();
-	
+
 	$scope.setEntry = function(comment, mood) {
 		happyService.setEntry(comment);
-		
+
 		if (mood.angry === true) {
-			
+		   console.log('');
 		}
 	}
-	
+
 });
