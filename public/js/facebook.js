10483461f0c46eafdbaf2912907e61dfd256a478
James <lawson.james.e@gmail.com>
2017-05-23 11:07:46 -0400

    so much testing

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -69,6 +69,7 @@
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
+        console.log(response.email);
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
