a7b34b56c8351e789f95d8b4cc8f1fb7583d5cc4
James <lawson.james.e@gmail.com>
2017-05-23 16:13:11 -0400

    login form posts data to database and username validation

--- public/js/loginCtrl.js
+++ public/js/loginCtrl.js
@@ -1,5 +1,30 @@
 var app = angular.module('happyMod');
 
 app.controller("loginController", function($scope, happyService) {
-       
-});
+
+    $scope.addUser = function(user) {
+        
+        $scope.user = user;
+        console.log($scope.user);
+        
+        happyService.userPromise().then(function(data){
+            var match = false;
+            data.forEach(function(index){
+                if (index.username === $scope.user.name){
+ return match = true; } 
+                });
+                         if (match === true){
+                console.log('username taken')
+            } else {
+                happyService.addUser(user).then(function() {
+        });
+            };
+            });
+    };
+         
+        
+     	
+  
+    
+    
+    });
