1a9eba86dbfe0e7bf988cb1883865c2b954b6088
Brian Barnes <barnesb7@gmail.com>
2017-05-23 11:52:08 -0400

    loginfactory

--- public/js/loginCtrl.js
+++ public/js/loginCtrl.js
@@ -1,5 +1,8 @@
 var app = angular.module('happyMod');
 
-app.controller("loginController", function($scope, happyService) {
+app.controller("loginController", function($scope, loginFactory) {
+
+  //EVENTUALLY SEND USER INFO TO HOLDER
+    loginFactory.storeUserInfo(/*info to send set here as param*/);
        
 });
