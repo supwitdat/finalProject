0ec79e199fe7ba25533a73e306d2c4bf27ee4c41
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-25 14:22:23 -0400

    some styling

--- public/js/monthCtrl.js
+++ public/js/monthCtrl.js
@@ -4,6 +4,7 @@ var app = angular.module('happyMod');
 app.controller("monthController", function($scope, happyService) {
  	console.log("monthController has loaded");
 
+	
 	$scope.days = happyService.getDays();
 	
 	$scope.showEntry = function(day) {
